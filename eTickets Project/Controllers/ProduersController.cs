002f81f5777c70d071152d295d10a9b8a6524142
saksham-prog <sakshammittal534@gmail.com>
2023-06-07 18:47:56 +0530

    first commit

---
+++ eTickets Project/Controllers/ProduersController.cs
@@ -0,0 +1,22 @@
+﻿using eTickets_Project.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace eTickets_Project.Controllers
+{
+    public class ProduersController : Controller
+    { 
+
+         private readonly AppDbContext _context;
+
+           public ProduersController(AppDbContext context)
+           {
+               _context = context;
+           }
+    public async Task<IActionResult> Index()
+        {
+            var allproducers =await _context.Producers.ToListAsync();
+            return View(allproducers);
+        }
+    }
+}
